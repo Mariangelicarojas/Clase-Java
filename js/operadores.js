0e40c83af6e252c6fb3169e33da3a8003f904817
Maria Rojas <mariarojas792@gmail.com>
2023-10-22 18:00:30 -0300

    modifique operadores con multiplicación

--- js/operadores.js
+++ js/operadores.js
@@ -94,27 +94,33 @@ function sumar(valor1,valor2) {
 function restar(valor1,valor2) {
   return Number(valor1) - Number(valor2);        
 }
+function multiplicar(valor1,valor2){
+  return Number(valor1) * Number(valor2);  
+}
+
 function calcular(operacion) {
   let valor1 = getValueById('valor1');
   let valor2 = getValueById('valor2');
   let resultado = '';
+
+  if (valor1 !== '' && valor2 !== '') {
   
-  if(valor1 !== '' && valor2 !== '') {
-      //quiero saber que operacion realizar
-      if(operacion === '+') {
-          //invoco la funcion sumar
-          resultado = sumar(valor1,valor2);
-      }else {
-          resultado = restar(valor1,valor2);
-      }
+    if (operacion === '+') {
+      resultado = sumar(valor1, valor2);
+    } else if (operacion === '*') {
+      resultado = multiplicar(valor1, valor2);
+    } else {
+      resultado = restar(valor1, valor2);
+    }
+    actualizarContenido('res', resultado);
   }
-  actualizarContenido('res',resultado);
 }
 
 function getValueById(id) {
   //Si lo tengo que crear y devolver, entonces solo lo devuelvo
   return document.getElementById(id).value;
 }
+
 function actualizarContenido(id,nuevoContenido) {
   document.getElementById(id).innerHTML = nuevoContenido;
 }
